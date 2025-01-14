90c942c0b3f29e981b9e90049b1d643b84b89b55
Albin-Lundmark <abbelundmark@gmail.com>
2025-01-14 16:10:47 +0100

    Changed the way I fetch my data from contentful to use useStaticQuery filtered to a specific page.

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,15 +1,52 @@
 import React from 'react'
 import Layout from '../components/layout'
-import { graphql } from 'gatsby'
+import { useStaticQuery, graphql } from 'gatsby'
 import Seo from '../components/seo'
 import ProjectCard from '../components/project-card'
 
-const PortfolioPage = ({ data }) => {
-  const projects = data.allContentfulPortfolioProjects.nodes
+const PortfolioPage = () => {
+  const projectData = useStaticQuery(graphql`
+    query {
+      contentfulPortfolioPage(title: { eq: "Portfolio" }) {
+        title
+        heading
+        slug
+        pageText {
+          childrenMarkdownRemark {
+            html
+          }
+        }
+      }
+      allContentfulPortfolioProjects {
+        nodes {
+          createdAt
+          title
+          slug
+          id
+          preview
+          repo
+          projectImage {
+            description
+            gatsbyImageData(
+              placeholder: BLURRED
+              layout: CONSTRAINED
+              width: 800
+            )
+          }
+        }
+      }
+    }
+  `)
 
+  const { heading } = projectData.contentfulPortfolioPage
+
+  const projects = projectData.allContentfulPortfolioProjects.nodes.sort(
+    (a, b) => new Date(a.createdAt) - new Date(b.createdAt)
+  )
+  
   return (
     <Layout>
-      <h1 className='font-heading text-gray-800'>Min projektsida!</h1>
+      <h1 className='font-heading text-gray-800'>{heading}</h1>
       <ul className='grid grid-cols-1 mt-0 mx-auto pl-0 md:grid-cols-2 lg:grid-cols-3 md:gap-5'>
         {projects.map(project => (
           <ProjectCard key={project.id} project={project} />
@@ -20,21 +57,5 @@ const PortfolioPage = ({ data }) => {
 }
 
 export const Head = () => <Seo title='Portfolio'></Seo>
-
-export const query = graphql`
-  query {
-    allContentfulPortfolioProjects {
-      nodes {
-        title
-        slug
-        id
-        projectImage {
-          description
-          gatsbyImageData(placeholder: BLURRED, layout: CONSTRAINED, width: 800)
-        }
-      }
-    }
-  }
-`
 
 export default PortfolioPage
