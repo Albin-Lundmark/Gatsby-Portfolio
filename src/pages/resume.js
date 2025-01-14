7dbb6817344f0257513bd022a970530480082ddd
Albin-Lundmark <abbelundmark@gmail.com>
2025-01-14 16:13:16 +0100

    Changed the way I fetch data from contentful to keep it consistent on my different pages.

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -3,33 +3,37 @@ import Layout from '../components/layout'
 import { graphql } from 'gatsby'
 import Seo from '../components/seo'
 
-const ResumePage = ({ data }) => {
-  const pages = data.allContentfulPage.nodes[0]
-
-  return (
-    <Layout>
-      <h1 className='text-gray-800'>Min CV-sida!</h1>
-      <h2 className='text-gray-800'>{pages && pages.resume.title}</h2>
-    </Layout>
-  )
-}
-
-export const Head = () => <Seo title='Portfolio'></Seo>
-
-export const query = graphql`
-  query {
-    allContentfulPage {
-      nodes {
-        resume {
-          title
-          file {
-            fileName
-            url
+const ResumePage = () => {
+  const resumeData = graphql`
+    query {
+      contentfulPortfolioPage(title: { eq: "Resume" }) {
+        title
+        heading
+        slug
+        pageText {
+          childrenMarkdownRemark {
+            html
           }
         }
       }
     }
-  }
-`
+  `
+
+  const { heading, pageText } = resumeData.contentfulPortfolioPage
+
+  return (
+    <Layout>
+      <h1 className='font-heading text-gray-800'>{heading}</h1>
+      <p
+        className='prose max-w-none'
+        dangerouslySetInnerHTML={{
+          __html: pageText.childrenMarkdownRemark.html
+        }}
+      ></p>
+    </Layout>
+  )
+}
+
+export const Head = () => <Seo title='Resume'></Seo>
 
 export default ResumePage
