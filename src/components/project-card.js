48c262b606246845f620a21fbefd7a84b37d9409
Albin-Lundmark <abbelundmark@gmail.com>
2025-01-14 16:07:11 +0100

    Applied my custom hook to check for window size to show correct image.

--- src/components/project-card.js
+++ src/components/project-card.js
@@ -1,21 +1,53 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import { getImage, GatsbyImage } from 'gatsby-plugin-image'
+import { useWindowSize } from '../hooks/useWindowSize'
 
 const ProjectCard = ({ project }) => {
-  const image = getImage(project.projectImage[1])
+  const desktopImage = getImage(project.projectImage[0])
+  const mobileImage = getImage(project.projectImage[1])
+  const isMobile = useWindowSize()
 
   return (
-    <li className='mt-3 mb-7'>
-      <div className='flex flex-col text-nowrap'>
-          <GatsbyImage
-            image={image}
-            alt={project.title}
-            className='h-full w-full object-cover rounded-lg'
-          />
-            <Link to={`/projects/${project.slug}`}>{project.title}</Link>
-            <Link to={project.repo}>{`${project.title} Repo`}</Link>
-            <Link to={project.preview}>{`Live preview`}</Link>
+    <li className='mx-auto mt-3 mb-7 pl-0'>
+      <div className='flex flex-col h-full md:h-auto shadow border-2 border-gray-200 rounded-lg p-3'>
+        <Link
+          className='text-center font-body font-semibold mb-2'
+          to={`/projects/${project.slug}`}
+        >
+          {project.title}
+          {isMobile ? (
+            <GatsbyImage
+              image={mobileImage}
+              alt={project.title}
+              className='h-full w-full object-cover rounded-lg'
+            />
+          ) : (
+            <GatsbyImage
+              image={desktopImage}
+              alt={project.title}
+              className='h-full w-full object-cover rounded-lg'
+            />
+          )}
+        </Link>
+        <div className='flex flex-col text-center gap-2'>
+          {project.repo && (
+            <a
+              className='text-body px-4 py-2 bg-emerald-100 hover:bg-emerald-200 hover:text-gray-800 rounded-lg'
+              href={project.repo}
+              rel='noreferrer'
+              target='_blank'
+            >{`Github repo`}</a>
+          )}
+          {project.preview && (
+            <a
+              className='text-body px-4 py-2 hover:bg-gray-100 hover:text-gray-800 rounded-lg'
+              href={project.preview}
+              rel='noreferrer'
+              target='_blank'
+            >{`Live preview`}</a>
+          )}
+        </div>
       </div>
     </li>
   )
