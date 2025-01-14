448cee5949788d7c1edfa4e519606ed6dce7ac3a
Albin-Lundmark <abbelundmark@gmail.com>
2025-01-14 16:08:34 +0100

    Imported data from contentful for text on my homepage.

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,39 @@ import { Link } from 'gatsby'
 import React from 'react'
 import Layout from '../components/layout'
 import Seo from '../components/seo'
+import { graphql, useStaticQuery } from 'gatsby'
 
-const IndexPage = () => (
-  <Layout>
-    <h1 className='font-heading'>Min första gatsby sida!</h1>
-    <Link to='/portfolio'>Besök mina projekt!</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  const pageData = useStaticQuery(graphql`
+    query {
+      contentfulPortfolioPage(title: { eq: "Home" }) {
+        heading
+        slug
+        pageText {
+          childrenMarkdownRemark {
+            html
+          }
+        }
+      }
+    }
+  `)
 
+  const { heading, pageText } = pageData.contentfulPortfolioPage
+  return (
+    <Layout>
+      <h1 className='font-heading text-gray-800'>{heading}</h1>
+      {pageText && (
+        <div
+          className='prose max-w-none'
+          dangerouslySetInnerHTML={{
+            __html: pageText.childrenMarkdownRemark.html
+          }}
+        />
+      )}
+      <Link to='/portfolio'>Besök mina projekt!</Link>
+    </Layout>
+  )
+}
 export const Head = () => <Seo title='Hem'></Seo>
 
 export default IndexPage
