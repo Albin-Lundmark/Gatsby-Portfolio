43b4c4a9ac8287ca2f52ff452f7f52903d4d7067
Albin-Lundmark <abbelundmark@gmail.com>
2025-01-15 13:27:38 +0100

    Making sure to have tailwind check every file in my src directory to keep styles.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,8 @@ module.exports = {
   content: [
     `./src/pages/**/*.{js,jsx,ts,tsx}`,
     `./src/components/**/*.{js,jsx,ts,tsx}`,
-    `./src/templates/**/*.{js,jsx,ts,tsx}`
+    `./src/templates/**/*.{js,jsx,ts,tsx}`,
+    `./src**/*.{js,jsx,ts,tsx}`
   ],
   theme: {
     extend: {
