0b5961546f7980c5ceb52188c84bfb9ac49c4fe7
Albin-Lundmark <abbelundmark@gmail.com>
2025-01-15 13:28:32 +0100

    Added z-index to menu to always keep it on top of content if opened.

--- src/components/menu.js
+++ src/components/menu.js
@@ -38,7 +38,7 @@ const Menu = () => {
 
   return (
     <nav
-      className='w-full fixed top-0 bg-emerald-100'
+      className='w-full fixed top-0 bg-emerald-100 z-10'
       role='navigation'
       aria-label='Main navigation'
     >
