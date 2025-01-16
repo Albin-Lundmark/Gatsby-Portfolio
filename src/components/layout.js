936bbb1618edced3e7067c17d7d65fcb578b8eaa
Albin-Lundmark <abbelundmark@gmail.com>
2025-01-16 20:36:27 +0100

    Moved loadingscreen component to be used in layout assuring it shows the first time anyone enters my portfolio despite not landing on the homepage.

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import Menu from './menu'
 import Footer from './footer'
+import LoadingScreen from './loadingscreen'
 
 const Layout = ({ children }) => {
   return (
     <>
+      <LoadingScreen />
       <header className='mb-24'>
         <Menu />
       </header>
-      <main className='max-w-full prose mx-3 md:mx-10 lg:mx-10'>{children}</main>
+      <main className='max-w-full prose mx-4 md:mx-12 lg:mx-24 xl:mx-32 2xl:mx-64'>
+        {children}
+      </main>
       <footer className='max-w-full'>
         <Footer />
       </footer>
