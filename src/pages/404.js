1c9cb58b6770dd40debfa0802c5e15be0780ab04
Albin-Lundmark <abbelundmark@gmail.com>
2025-01-16 15:51:29 +0100

    Changed text to be in english.

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,13 +6,12 @@ const NotFoundPage = () => {
   return (
     <Layout>
       <div className='text-center'>
-        <h1 className='font-heading'>Hoppsan, nu hamnade du utanför mina sidor</h1>
-        <Link className='font-body' to='/'>Klicka här för att komma tillbaka till hemsidan</Link>
+        <h1 className='font-heading'>Oops, this page doesn't seem to exist yet</h1>
+        <Link className='font-body' to='/'>Click here to return to my homepage</Link>
       </div>
     </Layout>
   )
 }
+export const Head = () => <Seo title='Page not found'></Seo>
 
 export default NotFoundPage
-
-export const Head = () => <title>Hoppsan</title>
