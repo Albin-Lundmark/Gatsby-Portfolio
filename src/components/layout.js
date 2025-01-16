0a23c5a5f69c1556dec60773215678e2f8b6cc17
Albin-Lundmark <abbelundmark@gmail.com>
2025-01-16 13:55:31 +0100

    Adding some meta data for SEO to my seo component and removing html tag from layout component for now.

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,15 +4,15 @@ import Footer from './footer'
 
 const Layout = ({ children }) => {
   return (
-    <html lang='en'>
-      <header className='mb-20'>
+    <>
+      <header className='mb-24'>
         <Menu />
       </header>
       <main className='max-w-full prose mx-3 md:mx-6 lg:mx-12'>{children}</main>
       <footer className='max-w-full'>
         <Footer />
       </footer>
-    </html>
+    </>
   )
 }
 
--- src/components/seo.js
+++ src/components/seo.js
@@ -9,15 +9,22 @@ const Seo = ({ title }) => {
           description
           siteUrl
           title
+          author
         }
       }
     }
   `)
 
   return (
-    <title>
-      {title} | {data.site.siteMetadata.title}
-    </title>
+    <head>
+      <meta charSet='utf-8' />
+      <meta name='viewport' content='width=device-width, initial-scale=1' />
+      <meta name='description' content={data.site.siteMetadata.description} />
+      <meta name='author' content={data.site.siteMetadata.author} />
+      <title>
+        {title} | {data.site.siteMetadata.title}
+      </title>
+    </head>
   )
 }
 
