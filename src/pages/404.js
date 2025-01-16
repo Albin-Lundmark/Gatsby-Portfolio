cb5ca998eb314823ba21248949147c8f640b4490
Albin-Lundmark <abbelundmark@gmail.com>
2025-01-16 15:53:24 +0100

    Added missing import for Seo component.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/layout'
+import Seo from '../components/seo'
 
 const NotFoundPage = () => {
   return (
     <Layout>
       <div className='text-center'>
-        <h1 className='font-heading'>Oops, this page doesn't seem to exist yet</h1>
-        <Link className='font-body' to='/'>Click here to return to my homepage</Link>
+        <h1 className='font-heading'>
+          Oops, this page doesn't seem to exist yet
+        </h1>
+        <Link className='font-body' to='/'>
+          Click here to return to my homepage
+        </Link>
       </div>
     </Layout>
   )
