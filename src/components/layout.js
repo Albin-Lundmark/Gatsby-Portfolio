dba32ab6b7a1a7a47840df204876592d0f5b93b9
Albin-Lundmark <abbelundmark@gmail.com>
2025-01-16 21:44:02 +0100

    Adjusted paddings to make my site not look too crowded and give a more cosy feeling.

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,10 +6,10 @@ import LoadingScreen from './loadingscreen'
 const Layout = ({ children }) => {
   return (
     <>
-      <header className='mb-10'>
+      <header className='max-w-full'> 
         <Menu />
       </header>
-      <main className='max-w-full prose mx-4 md:mx-12 lg:mx-24 xl:mx-32 2xl:mx-64'>
+      <main className='max-w-full prose py-20 mx-4 md:mx-12 lg:mx-24 xl:mx-32 2xl:mx-64'>
         {children}
       </main>
       <footer className='max-w-full'>
